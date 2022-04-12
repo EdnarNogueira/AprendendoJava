89fe53f17c490cd713f59c5f4f1807b1115a760f
Ednar Nogueira <ednaredms@gmail.com>
2022-04-11 21:55:45 -0300

    Adicionando tipos de dados

---
+++ src/aprendendojava/tiposDeDados.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aprendendojava;
+
+/**
+ *
+ * @author ednar
+ */
+public class tiposDeDados {
+    public static void main(String[] args) {
+        int num;
+        float b;
+        char c;
+        String s;
+
+        //integer inteiro
+        num = 100;
+        //float ponto flutuante 
+        b = 1.234f;
+        //character caractere
+        c = 'A';
+        //string  cadeia de caractere
+        s = "Ola Mundo aqui é uma string";
+
+        System.out.println("IMPRESSO NUMERO INTEIRO: " + num);
+        System.out.println("NUMERO REAL: " + b);
+        System.out.println("CARACTERE : " + c);
+        System.out.println("VALOR STRING: " + s);
+
+    }
+}
