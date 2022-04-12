0dff657ce677ea6dcb53fb3efd9381f809b5c01f
Ednar Nogueira <ednaredms@gmail.com>
2022-04-11 21:51:47 -0300

    Adicionando hello word

--- src/aprendendojava/AprendendoJava.java
+++ src/aprendendojava/AprendendoJava.java
@@ -1,20 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package aprendendojava;
-
-/**
- *
- * @author ednar
- */
 public class AprendendoJava {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
--- src/aprendendojava/java01.java
+++ src/aprendendojava/java01.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package aprendendojava;
-
-/**
- *
- * @author ednar
- */
 public class java01 {
-    
+    public static void main(String[] args) {
+        System.out.println("OLA MUNDO"); 
+        System.out.println("HELLO WORD");
+    }
+ 
 }
